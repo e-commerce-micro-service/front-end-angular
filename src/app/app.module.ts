563de23697dba30ed65b99747fa5d585e061c6e0
cosimo <cpolito07@gmail.com>
2021-06-10 12:00:43 +0200

    aggiunta sicurezza

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,47 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {WelcomeComponent} from "./welcome/welcome.component";
+import {NavbarComponent} from "./navbar/navbar.component";
+import {FooterComponent} from "./footer/footer.component";
+import {LoginComponent} from "./login/login.component";
+// import {AutomobileSearchComponent} from "./automobile/automobile-search/automobile-search.component";
+// import {AutomobileSearchResultsComponent} from "./automobile/automobile-search-results/automobile-search-results.component";
+// import {AutomobileCreateComponent} from "./automobile/automobile-create/automobile-create.component";
+// import {AutomobileDetailComponent} from "./automobile/automobile-detail/automobile-detail.component";
+// import {AutomobileEditComponent} from "./automobile/automobile-edit/automobile-edit.component";
+// import {AutomobileDeleteComponent} from "./automobile/automobile-delete/automobile-delete.component";
+import {HTTP_INTERCEPTORS, HttpClientModule} from "@angular/common/http";
+import {JwtInterceptor} from "./auth/jwt.interceptor";
+import {AuthInterceptor} from "./auth/auth.interceptor";
+import {DatePipe} from "@angular/common";
+import {FormsModule} from "@angular/forms";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WelcomeComponent,
+    FooterComponent,
+    NavbarComponent,
+    LoginComponent,
+    // AutomobileSearchComponent,
+    // AutomobileSearchResultsComponent,
+    // AutomobileCreateComponent,
+    // AutomobileDetailComponent,
+    // AutomobileEditComponent,
+    // AutomobileDeleteComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    FormsModule,
+    AppRoutingModule,
+    HttpClientModule,
+   ],
+  providers: [
+    { provide: HTTP_INTERCEPTORS, useClass: JwtInterceptor, multi: true },
+    { provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true },
+    DatePipe
   ],
-  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
